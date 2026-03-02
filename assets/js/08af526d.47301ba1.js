60922b687b6dd99b41f1c47681ca4bf79c581aba
gh-actions <actions@github.com>
2026-03-02 12:36:11 +0000

    Deploy website - based on e1ed6556f7247aff4eb3c2676c945430980a6b3d

--- assets/js/08af526d.47301ba1.js
+++
@@ -1,1 +0,0 @@
-"use strict";(globalThis.webpackChunkfluss_website=globalThis.webpackChunkfluss_website||[]).push([[529],{41344(e){e.exports=JSON.parse('{"metadata":{"permalink":"/blog","page":1,"postsPerPage":10,"totalPages":2,"totalCount":17,"nextPage":"/blog/page/2","blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
---
+++ assets/js/ea70c07d.32d1ef46.js
@@ -0,0 +1,1 @@
+"use strict";(globalThis.webpackChunkfluss_website=globalThis.webpackChunkfluss_website||[]).push([[8732],{76387(e){e.exports=JSON.parse('{"metadata":{"permalink":"/blog/page/2","page":2,"postsPerPage":10,"totalPages":2,"totalCount":18,"previousPage":"/blog","blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
